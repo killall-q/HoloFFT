dca2c78f84e74e0a2242083fd055e8df7185e630
Joshua Fan <killall-q@users.noreply.github.com>
2016-04-02 12:06:24 -0700

    Improved randomness of distribution

--- @Resources/Presets/Grid.lua
+++ @Resources/Presets/Grid.lua
@@ -2,8 +2,8 @@ local bands, b, rows, r, v = ...
 
 -- YOUR CODE HERE
 local br = math.floor(bands^0.5)
-local x = math.floor(b / bands * br) / br * 2 + v % 0.1 - 1
-local y = (b % br) / br * 2 + v % 0.08 - 1
+local x = math.floor(b / bands * br) / br * 2 + v % 0.01 * 10 - 1
+local y = (b % br) / br * 2 + v % 0.008 * 12 - 1
 local z = math.sin(r / rows * 3.14) * v * 2 - 1
 -- YOUR CODE HERE
 
